8c4cf13ec2b9c1d650b24942214bd0af61cd378c
Gábor Fülep <gfulep@diligent.com>
2021-12-13 11:19:34 +0100

    Template test added

---
+++ Tests/Template/Tests.cs
@@ -0,0 +1,34 @@
+﻿namespace Tests.Template;
+
+/*
+public class Tests
+{
+    private static readonly IEnumerable<string> input = @"".Split(Environment.NewLine);
+
+    [Fact]
+    public void TestFirstPart()
+    {
+        // Arrange
+        var solver = new Solver(input);
+
+        // Act
+        var result = solver.SolvePartOne();
+
+        // Assert
+        result.Should().Be("");
+    }
+
+    [Fact]
+    public void TestSecondPart()
+    {
+        // Arrange
+        var solver = new Solver(input);
+
+        // Act
+        var result = solver.SolvePartTwo();
+
+        // Assert
+        result.Should().Be("");
+    }
+}
+*/

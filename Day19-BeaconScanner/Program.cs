05e99a5af11847c709b07c0a9b59943212cdf5e9
Gábor Fülep <gfulep@diligent.com>
2021-12-19 22:19:06 +0100

    add timing

--- Day19-BeaconScanner/Program.cs
+++ Day19-BeaconScanner/Program.cs
@@ -1,6 +1,10 @@
 ﻿using Day19_BeaconScanner;
+using System.Diagnostics;
 
+var sw = Stopwatch.StartNew();
 var solver = new Solver(File.ReadLines("input.txt"));
 Console.WriteLine(solver.SolvePartOne());
+sw.Stop();
+Console.WriteLine(sw.ElapsedMilliseconds.ToString() + "ms");
 var solver2 = new Solver(File.ReadLines("input.txt"));
 Console.WriteLine(solver2.SolvePartTwo());
